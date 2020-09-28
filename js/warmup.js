47df3fe175abce5b084542fb50300a5224505d0b
William Tisdale <will.tisdale.31@gmail.com>
2020-09-28 09:34:40 -0500

    updated warmup files

--- js/warmup.js
+++ js/warmup.js
@@ -1,12 +1,19 @@
 "use strict";
 
-console.log("Hello, William!");
-
+/* console.log("Hello, William!");
 console.log("Hello, Johnny!");
-
 console.log("Hello, Kelly!");
-
 console.log("Hello, Clay!");
+console.log("Hello, Daniel!");*/
 
-console.log("Hello, Daniel!");
+function helloName(name){
+    console.log("Hello " + name + "!");
+    //we will talk about returning later!
+}
+
+helloName("William");
+helloName("Johnny");
+helloName("Kelly");
+helloName("Clay");
+helloName("Daniel");
 
