8a6416c4dbd88c52557b91111a63e8a4ebbd19ee
William Tisdale <will.tisdale.31@gmail.com>
2020-10-05 09:09:55 -0500

    Monday 10/05 warmup

--- js/warmup.js
+++ js/warmup.js
@@ -6,14 +6,25 @@ console.log("Hello, Kelly!");
 console.log("Hello, Clay!");
 console.log("Hello, Daniel!");*/
 
-function helloName(name){
-    console.log("Hello " + name + "!");
-    //we will talk about returning later!
+// function helloName(name){
+//     console.log("Hello " + name + "!");
+//     //we will talk about returning later!
+// }
+//
+// helloName("William");
+// helloName("Johnny");
+// helloName("Kelly");
+// helloName("Clay");
+// helloName("Daniel");
+
+function allTheSame(input){
+    if(input.toLowerCase() === input || input.toUpperCase() === input){
+        return true
+    } else {
+        return false
+    }
 }
 
-helloName("William");
-helloName("Johnny");
-helloName("Kelly");
-helloName("Clay");
-helloName("Daniel");
-
+console.log(allTheSame("hello world"))
+console.log(allTheSame("HELLO WORLD"))
+console.log(allTheSame("HeLLo wORld"))
