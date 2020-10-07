d90b280336eb59213bb0600b1feabc3025724dfb
William Tisdale <will.tisdale.31@gmail.com>
2020-10-07 11:29:30 -0500

    updated coding challenges

---
+++ js/amateur_coding_challenges.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//4
+// function replaceLettersWithNumbers(input, character){
+//     var newArray = input.split("");
+//     newArray.forEach(function(element){
+//         if(element === )
+//     })
+// }
