fcc790c687633306f1f47f7025e4fad1e169453d
William Tisdale <will.tisdale.31@gmail.com>
2020-10-07 12:34:45 -0500

    objects update

--- js/amateur_coding_challenges.js
+++ js/amateur_coding_challenges.js
@@ -2,8 +2,17 @@
 
 //4
 // function replaceLettersWithNumbers(input, character){
-//     var newArray = input.split("");
-//     newArray.forEach(function(element){
-//         if(element === )
-//     })
+//     var newArray = [];
+//     var count = {};
+//     var ch, index, len, count;
+//     for(var i = 0; i < input.length; i++){
+//         ch = input.charAt(i);
+//         count[ch] = count ? count + 1 : 1;
+//     }
+//     for (ch in count){
+//         newArray.push(count[ch]);
+//     }
+//     return newArray
 // }
+//
+// console.log(replaceLettersWithNumbers('hello world'));
