fe31c8c77dd862ed6a96a7d0f61265ca40db8fb0
William Tisdale <will.tisdale.31@gmail.com>
2020-10-08 14:58:54 -0500

    aphorisms commit

--- js/amateur_coding_challenges.js
+++ js/amateur_coding_challenges.js
@@ -1,18 +1,67 @@
 "use strict";
 
-//4
-// function replaceLettersWithNumbers(input, character){
-//     var newArray = [];
-//     var count = {};
-//     var ch, index, len, count;
-//     for(var i = 0; i < input.length; i++){
-//         ch = input.charAt(i);
-//         count[ch] = count ? count + 1 : 1;
+//1
+// function sum(start, finish, step){
+//     var sum = 0;
+//     for(var i = start; i <= finish; i += step){
+//         sum += i;
 //     }
-//     for (ch in count){
-//         newArray.push(count[ch]);
-//     }
-//     return newArray
+//     return sum
 // }
 //
-// console.log(replaceLettersWithNumbers('hello world'));
+// console.log(sum(1, 4, 1));
+// console.log(sum(4, 10, 5));
+
+//2
+
+// function containsEnglish(string){
+//     var newStr = string.toLowerCase();
+//     var word = "english"
+//     return newStr.includes(word);
+// }
+//
+// console.log(containsEnglish("FaagdnglishAGG"));
+// console.log(containsEnglish("SMFENglishasnD"));
+
+//3
+
+// function positiveAndNegative(array){
+//     var neg = 0;
+//     var pos = 0;
+//     var arr = [];
+//     if(array === undefined){
+//         return arr
+//     }
+//     array.forEach(function(number){
+//         if(number < 0){
+//             neg += number
+//         } else {
+//             pos += 1
+//         }
+//     })
+//     arr.push(pos);
+//     arr.push(neg);
+//     return arr
+// }
+//
+// console.log(positiveAndNegative([1, -6, 5, 4, 3, -7, -10, 201, -3]));
+// console.log(positiveAndNegative());
+
+//4
+function replaceLettersWithNumbers(input, character){
+    var arr = input.split("")
+    var myMap = new Map();
+    var newArr = []
+    arr.forEach(function(element){
+        myMap.set(element);
+    })
+    arr.forEach(function(element){
+        if(element === myMap){
+            element += 1
+            newArr.push(element)
+        }
+    })
+    return myMap
+}
+
+console.log(replaceLettersWithNumbers('hello world'));
