6da40fb4f8dd8cd0ee07cb9f8b9254844667aff5
William Tisdale <will.tisdale.31@gmail.com>
2020-11-03 15:57:27 -0600

    completed dom event exercise

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -2,13 +2,17 @@
 var timer = document.getElementById('timer')
 var count = 1
 var intervalId;
-var timeArr = [0,59,55]
+var timeArr = [0,0,0,0]
 
 var start = document.getElementById("start")
 start.addEventListener('click', startTimer)
 function startTimer(){
     console.log("start");
     intervalId = setInterval(function (){
+        if(timeArr[3] > 9){
+            timeArr[2] += count
+            timeArr[3] = 0
+        }
         if(timeArr[2] > 59){
             timeArr[1] += count
             timeArr[2] = 0
@@ -19,8 +23,8 @@ function startTimer(){
             timeArr[2] = 0
         }
         timer.innerHTML = timeArr.join(':')
-        timeArr[2] += count
-    },1000)
+        timeArr[3] += count
+    },100)
 }
 
 var pause = document.getElementById("pause")
@@ -35,6 +39,6 @@ reset.addEventListener('click', resetTimer)
 function resetTimer(){
     console.log('reset');
     clearInterval(intervalId)
-    timeArr = [0,0,0]
+    timeArr = [0,0,0,0]
     timer.innerHTML = timeArr.join(':')
 }
