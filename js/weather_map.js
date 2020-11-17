f5c446cb0ea2937297a331e018a2f80c689324b5
William Tisdale <will.tisdale.31@gmail.com>
2020-11-17 12:00:02 -0600

    completed basic functionality for 5 day forecast

--- js/weather_map.js
+++ js/weather_map.js
@@ -29,18 +29,28 @@ $(document).ready(function(){
             windDirection(data)
             renderCard()
         });
+    }
 
+    // currentWeather()
+
+    function forecast() {
+        $.get("https://api.openweathermap.org/data/2.5/onecall", {
+            APPID: OPEN_WEATHER_APPID,
+            lat: 29.423017,
+            lon: -98.48527,
+            units: "imperial",
+            exclude: "current,minutely,hourly,alerts"
+        }).done(function (data) {
+            console.log(data);
+            renderCards(data)
+        });
 
     }
 
-    currentWeather()
-
-
+    forecast()
 
     function renderCard(){
-
         let cardHTML = "";
-
         cardHTML += "<div class='col'><div class='card'>"
         cardHTML += "<div id='dayOfTheWeek' class='card-header text-center'>"
         cardHTML += today + "</div>"
@@ -53,6 +63,31 @@ $(document).ready(function(){
         cardHTML += "<p>" + "Pressure: <strong>" + pressure + "</strong></p>"
         cardHTML += "</div></div></div>"
 
+        $('#card-row').html(cardHTML)
+    }
+
+    function renderCards(data){
+        let cardHTML = "";
+        for(var i = 0; i < 5; i++){
+            max = data.daily[i].temp.max;
+            min = data.daily[i].temp.min;
+            description = data.daily[i].weather[0].description
+            humidity = data.daily[i].humidity
+            wind = data.daily[i].wind_speed
+            pressure = data.daily[i].pressure
+            windDirections(data, i)
+            cardHTML += "<div class='col'><div class='card'>"
+            cardHTML += "<div id='dayOfTheWeek' class='card-header text-center'>"
+            cardHTML += today + "</div>"
+            cardHTML += "<div class='card-body'>"
+            cardHTML += "<p class='text-center'>" + min + "°F / " + max + "°F"
+            cardHTML += "<hr>"
+            cardHTML += "<p>" + "Description: <strong>" + description + "</strong></p>"
+            cardHTML += "<p>" + "Humidity: <strong>" + humidity + "</strong></p>"
+            cardHTML += "<p>" + "Wind: <strong>" + wind + " " + windDir + "</strong></p>"
+            cardHTML += "<p>" + "Pressure: <strong>" + pressure + "</strong></p>"
+            cardHTML += "</div></div></div>"
+        }
         $('#card-row').html(cardHTML)
     }
 
@@ -117,6 +152,41 @@ $(document).ready(function(){
             } else if (data.wind.deg >= 303.75 && data.wind.deg <= 326.25) {
                 windDir = "NW";
             } else if (data.wind.deg > 326.25 && data.wind.deg < 348.75) {
+                windDir = "NNW";
+            }
+    }
+    function windDirections(data, i) {
+            if (data.daily[i].wind_deg > 348.75 || data.daily[i].wind_deg < 11.25) {
+                windDir = "N";
+            } else if (data.daily[i].wind_deg >= 11.25 && data.daily[i].wind_deg < 33.75) {
+                windDir = "NNE";
+            } else if (data.daily[i].wind_deg >= 33.75 && data.daily[i].wind_deg <= 56.25) {
+                windDir = "NE";
+            } else if (data.daily[i].wind_deg > 56.25 && data.daily[i].wind_deg < 78.75) {
+                windDir = "ENE";
+            } else if (data.daily[i].wind_deg >= 78.75 && data.daily[i].wind_deg <= 101.25) {
+                windDir = "E";
+            } else if (data.daily[i].wind_deg > 101.25 && data.daily[i].wind_deg < 123.75) {
+                windDir = "ESE";
+            } else if (data.daily[i].wind_deg >= 123.75 && data.daily[i].wind_deg <= 146.25) {
+                windDir = "SE";
+            } else if (data.daily[i].wind_deg > 146.25 && data.daily[i].wind_deg < 168.75) {
+                windDir = "SSE";
+            } else if (data.daily[i].wind_deg >= 168.75 && data.daily[i].wind_deg <= 191.25) {
+                windDir = "S";
+            } else if (data.daily[i].wind_deg > 191.25 && data.daily[i].wind_deg < 213.75) {
+                windDir = "SSW";
+            } else if (data.daily[i].wind_deg >= 213.75 && data.daily[i].wind_deg <= 236.25) {
+                windDir = "SW";
+            } else if (data.daily[i].wind_deg > 236.25 && data.daily[i].wind_deg < 258.75) {
+                windDir = "WSW";
+            } else if (data.daily[i].wind_deg >= 258.75 && data.daily[i].wind_deg <= 281.25) {
+                windDir = "W";
+            } else if (data.daily[i].wind_deg > 281.25 && data.daily[i].wind_deg < 303.75) {
+                windDir = "WNW";
+            } else if (data.daily[i].wind_deg >= 303.75 && data.daily[i].wind_deg <= 326.25) {
+                windDir = "NW";
+            } else if (data.daily[i].wind_deg > 326.25 && data.daily[i].wind_deg < 348.75) {
                 windDir = "NNW";
             }
     }
